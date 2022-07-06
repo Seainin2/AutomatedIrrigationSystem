240f4b79f65b5c366816086ffde71a7368270d3a
Seainin Keenan <101061636+Seainin2@users.noreply.github.com>
2022-07-06 13:00:07 +0000

    added code

--- arduino.py
+++ arduino.py
@@ -1,4 +1,15 @@
 from pyfirmata import Arduino
 import time
 
+board = Arduino("COM6")
+flag = True
+num = 1
 
+while(flag):
+  board.digital[13].write(num)
+  time.sleep(1)
+  if num == 1:
+    num = 0
+  if num == 0:
+    num = 1
+  
